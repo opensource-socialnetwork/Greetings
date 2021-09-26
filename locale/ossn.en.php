97dce46f626d4109e2917ccc78d0af3192454131
Arsalan Shah <manactric@gmail.com>
2021-09-27 01:54:12 +0500

    2.0

--- locale/ossn.en.php
+++ locale/ossn.en.php
@@ -2,14 +2,15 @@
 /**
  * Open Source Social Network
  *
- * @package   (softlab24.com).ossn
- * @author    OSSN Core Team <info@softlab24.com>
- * @copyright (C) SOFTLAB24 LIMITED
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@openteknik.com>
+ * @copyright (C) OpenTeknik LLC
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
 
 $en = array(
+	'greetings:welcomeback' => 'Welcome Back, %s!',
 	'greetings:morning' => 'Good Morning!',
 	'greetings:afternoon' => 'Good Afternoon!',
 	'greetings:evening' => 'Good Evening!',
